fddac0b7c1baa2f9f5ac6a25d966338e9176f503
marco4512 <marco_alex45@hotmail.com>
2023-01-31 20:38:28 -0600

    subiendo primer webhook con openai

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { Configuration, OpenAIApi } = require("openai");
 const app = express();
 const {WebhookClient} = require('dialogflow-fulfillment');
-
+const configuration = new Configuration({
+  apiKey:'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',
+});
+const openai = new OpenAIApi(configuration);
 const port = process.env.PORT || 3000;
 
 // for parsing json
@@ -28,6 +32,7 @@ app.post("/webhook", express.json(), (req, res) => {
   const agent = new WebhookClient({ request:req, response:res });
   console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
+  
   function welcome(agent) {
     agent.add(`Welcome to my agent!`);
   }
@@ -35,8 +40,20 @@ app.post("/webhook", express.json(), (req, res) => {
     agent.add(`I didn't understand`);
     agent.add(`I'm sorry, can you try again?`);
   }
-  function PruebaWeb(agent) {
-    agent.add(`Respuesta desde webHook`);
+  async function PruebaWeb(agent) {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `Q:Como accedo a Gmail
+               A:`,
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stop: ["Q:"],
+    });
+    //console.log(response.data.choices[0].text)
+    agent.add(response.data.choices[0].text);
   }
   let intentMap = new Map();
   intentMap.set('PruebaWeb', PruebaWeb);
