a61ddf6d8d41ac5143435cdc608bfd3ad09fb2c0
marco4512 <marco_alex45@hotmail.com>
2023-02-02 21:50:48 -0600

    Agregando un switch

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ app.post("/webhook", express.json(), (req, res) => {
   let pregunta = req.body['queryResult']['queryText'];
   var intencion = req.body['queryResult']['intent']['displayName']
   const agent = new WebhookClient({ request: req, response: res });
+  if(intencion=='Default_Fallback_Intent'){
   retornar_respuesta(pregunta).then(function (result) {
     console.log('entrando en la promesa')
     function fallback(agent) {
@@ -58,7 +59,7 @@ app.post("/webhook", express.json(), (req, res) => {
     let intentMap = new Map();
     intentMap.set('Default_Fallback_Intent', fallback);
     agent.handleRequest(intentMap);
-  })
+  })}
 
 });
 
