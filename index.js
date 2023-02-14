8781a929e908f55949e226c7b727d3fb54192617
marco4512 <marco_alex45@hotmail.com>
2023-02-13 21:21:09 -0600

    Nueva Configuracion por modulos

--- index.js
+++ index.js
@@ -34,39 +34,41 @@ app.get("/", (req, res) => {
 });
 /**Desde Aqui recibimos las peticiones de dialogFlow */
 app.post("/webhook", express.json(), (req, res) => {
-    const agent = new WebhookClient({ request: req, response: res });
-    var pregunta = req.body['queryResult']['queryText'];
-    var intencion = req.body['queryResult']['intent']['displayName']
-    //var respuestaOpenAi = openai_response(pregunta, intencion);
-    var parametros = req.body['queryResult']['parameters']
-    async function fallback(agent) {
-        return new Promise( resolve =>{
-            resolve(agent.add(`${ ResponderPreguta(pregunta)}`))
-        }) 
-    }
-    if (intencion == 'Default_Fallback_Intent') {
-        let intentMap = new Map();
-        intentMap.set('Default_Fallback_Intent', fallback);
-        setTimeout(agent.handleRequest(intentMap),4000)
-    } else {
-        var asesores = extraerAsesor(parametros.email)
-        Promise.all([asesores]).then(result => {
-            function enviarCorreoAsesor(agent) {
-                console.log(result.flat().length)
-                if (result.flat().length != 0) {
-                    agent.add(`Aqui estan tus asesores`)
-                    result.flat().map((asesor) => agent.add(`${asesor}`))
-                } else {
-                    agent.add(`:c No encuentro a tu asesor`)
-                }
-            }
+    Promise(() => {
+        const agent = new WebhookClient({ request: req, response: res });
+        var pregunta = req.body['queryResult']['queryText'];
+        var intencion = req.body['queryResult']['intent']['displayName']
+        //var respuestaOpenAi = openai_response(pregunta, intencion);
+        var parametros = req.body['queryResult']['parameters']
+        async function fallback(agent) {
+            return new Promise(resolve => {
+                resolve(agent.add(`${ResponderPreguta(pregunta)}`))
+            })
+        }
+        if (intencion == 'Default_Fallback_Intent') {
             let intentMap = new Map();
-            intentMap.set('enviarCorreoAsesor', enviarCorreoAsesor);
-            agent.handleRequest(intentMap);
-        }).catch(reason => {
-            console.log('Razon de que truene ->', reason)
-        })
-    }
+            intentMap.set('Default_Fallback_Intent', fallback);
+            agent.handleRequest(intentMap)
+        } else {
+            var asesores = extraerAsesor(parametros.email)
+            Promise.all([asesores]).then(result => {
+                function enviarCorreoAsesor(agent) {
+                    console.log(result.flat().length)
+                    if (result.flat().length != 0) {
+                        agent.add(`Aqui estan tus asesores`)
+                        result.flat().map((asesor) => agent.add(`${asesor}`))
+                    } else {
+                        agent.add(`:c No encuentro a tu asesor`)
+                    }
+                }
+                let intentMap = new Map();
+                intentMap.set('enviarCorreoAsesor', enviarCorreoAsesor);
+                agent.handleRequest(intentMap);
+            }).catch(reason => {
+                console.log('Razon de que truene ->', reason)
+            })
+        }
+    })
 });
 /**Mostrar la consola de manera local */
 app.listen(port, () => {
