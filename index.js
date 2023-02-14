f5e3b9c59ba4baba6dde8bf624248b734dc6d079
marco4512 <marco_alex45@hotmail.com>
2023-02-13 22:27:29 -0600

    Nueva Configuracion por modulos

--- index.js
+++ index.js
@@ -39,75 +39,16 @@ app.post("/webhook", express.json(), (req, res) => {
     var intencion = req.body['queryResult']['intent']['displayName']
     //var respuestaOpenAi = openai_response(pregunta, intencion);
     var parametros = req.body['queryResult']['parameters']
-    async function fallback(agent) {
-        const palabrasClave = ['gmail', 'drive', 'maps', 'docs', 'sheet', 'youtube',
-        'cloud', 'chrome', 'meet', 'calendario', 'formularios', 'formulario']
-        let newFormatQuestion = String(pregunta).toLocaleLowerCase().replace('?', '').replace('¿', '').trim()
-        let pregunta_separada = String(newFormatQuestion).split(' ')
-        let categoria = []
-        var newQuestion = {};
-        var respuesta='';
-        pregunta_separada.map(function (palabra) {
-            if (palabrasClave.includes(palabra)) {
-                categoria.push(palabra)
-            }
-        })
-        if (categoria.length != 0) {
-            let documento = categoria[0]
-            const docRef = doc(db, "Questions", documento);
-            const questionRef = collection(db, "Questions");
-            const docSnap = await getDoc(docRef);
-            if (docSnap.exists()) {
-                console.log('si extiste')
-                console.log(newFormatQuestion)
-                if (docSnap.data()[newFormatQuestion] != undefined) {
-                    agent.add(`${docSnap.data()[newFormatQuestion]}`)
-                    respuesta=docSnap.data()[newFormatQuestion]
-                    console.log(docSnap.data()[newFormatQuestion])
-                }
-                else {
-                    const response = await openai.createCompletion({
-                        model: "text-davinci-003",
-                        prompt: `Q:${newFormatQuestion}
-                           A:`,
-                        temperature: 0,
-                        max_tokens: 400,
-                        top_p: 1,
-                        frequency_penalty: 0,
-                        presence_penalty: 0,
-                        stop: ["Q:"],
-                    });
-                    agent.add(`${response.data.choices[0].text}`)
-                    respuesta=response.data.choices[0].text
-                    newQuestion[newFormatQuestion] = response.data.choices[0].text
-                    await updateDoc(doc(questionRef,documento), newQuestion);
-                    console.log('agregar a gmail')
-                }
-            } else {
-                console.log('No esta, tenemos que crearlo')
-                const response = await openai.createCompletion({
-                    model: "text-davinci-003",
-                    prompt: `Q:${newFormatQuestion}
-                       A:`,
-                    temperature: 0,
-                    max_tokens: 400,
-                    top_p: 1,
-                    frequency_penalty: 0,
-                    presence_penalty: 0,
-                    stop: ["Q:"],
-                });
-                agent.add(`${response.data.choices[0].text}`)
-                respuesta=response.data.choices[0].text;
-                newQuestion[newFormatQuestion] = response.data.choices[0].text;
-                await setDoc(doc(db, "Questions", documento), newQuestion)
-            }
+    Promise.all([ResponderPreguta(pregunta)]).then(res => {
+        async function fallback(agent) {
+            return ResponderPreguta(pregunta).then(res => {agent.add(`${res}`)})
         }
-        //return ResponderPreguta(pregunta).then(res => { agent.add(`${res}`) })
-    }
-    if (intencion == 'Default_Fallback_Intent') {
         let intentMap = new Map();
         intentMap.set('Default_Fallback_Intent', fallback)
         agent.handleRequest(intentMap)
+    })
+    if (intencion == 'Default_Fallback_Intent') {
+
     } else {
         var asesores = extraerAsesor(parametros.email)
         Promise.all([asesores]).then(result => {
