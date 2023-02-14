b8da274f6bb5346f32cd47b21c27719d7dff8e38
marco4512 <marco_alex45@hotmail.com>
2023-02-13 19:34:26 -0600

    Nueva Configuracion por modulos

--- fireBaseFunctios/consultarQuestions.js
+++ fireBaseFunctios/consultarQuestions.js
@@ -5,41 +5,7 @@ const configuration = new Configuration({
     apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
-async function extraerQuestions(pregunta) {
 
-    const docRef = doc(db, "Questions", "SomeQuestions");
-    const questionRef = collection(db, "Questions");
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-        if (docSnap.data()[pregunta] != undefined) {
-            console.log(docSnap.data()[pregunta])
-        } else {
-            const response = await openai.createCompletion({
-                model: "text-davinci-003",
-                prompt: `Q:${pregunta}
-                   A:`,
-                temperature: 0,
-                max_tokens: 100,
-                top_p: 1,
-                frequency_penalty: 0,
-                presence_penalty: 0,
-                stop: ["Q:"],
-            });
-            response.data.choices[0].text
-            console.log('Ingresando nueva Pregunta')
-            var newQuestion = {};
-            newQuestion[pregunta] = response.data.choices[0].text
-            await updateDoc(doc(questionRef, "SomeQuestions"), newQuestion);
-        }
-        //console.log("Document data:", docSnap.data());
-    }
-
-}
-
-function Formato(pregunta) {
-    let newFormat = String(pregunta).toLocaleLowerCase().replace('?', '').replace('¿', '')
-    console.log(newFormat)
-}
 const palabrasClave = ['gmail', 'drive', 'maps', 'docs', 'sheet', 'youtube',
     'cloud', 'chrome', 'meet', 'calendario', 'formularios', 'formulario']
 async function ResponderPreguta(pregunta) {
@@ -47,6 +13,7 @@ async function ResponderPreguta(pregunta) {
     let pregunta_separada = String(newFormatQuestion).split(' ')
     let categoria = []
     var newQuestion = {};
+    var respuesta='';
     pregunta_separada.map(function (palabra) {
         if (palabrasClave.includes(palabra)) {
             categoria.push(palabra)
@@ -61,6 +28,7 @@ async function ResponderPreguta(pregunta) {
             console.log('si extiste')
             console.log(newFormatQuestion)
             if (docSnap.data()[newFormatQuestion] != undefined) {
+                respuesta=docSnap.data()[newFormatQuestion]
                 console.log(docSnap.data()[newFormatQuestion])
             }
             else {
@@ -75,6 +43,7 @@ async function ResponderPreguta(pregunta) {
                     presence_penalty: 0,
                     stop: ["Q:"],
                 });
+                respuesta=response.data.choices[0].text
                 newQuestion[newFormatQuestion] = response.data.choices[0].text
                 await updateDoc(doc(questionRef,documento), newQuestion);
                 console.log('agregar a gmail')
@@ -92,20 +61,12 @@ async function ResponderPreguta(pregunta) {
                 presence_penalty: 0,
                 stop: ["Q:"],
             });
+            respuesta=response.data.choices[0].text;
             newQuestion[newFormatQuestion] = response.data.choices[0].text;
             await setDoc(doc(db, "Questions", documento), newQuestion)
         }
     }
+    return respuesta;
 }
-ResponderPreguta('que es google drive?')
-
-
-
-
-//Formato('¿Quien pinto la mona Lisa?')
-
-//export {extraerAsesor}
-//var preguntas = extraerQuestions('como hago una pizza ?');
-//Promise.all([preguntas]).then(resultado => {
-//    console.log(resultado)
-//})
+export{ResponderPreguta}
+//ResponderPreguta('que es google drive?')
